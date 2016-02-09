dcf8c92dbcfb66d562b8ad2e3ced275acf158d4c
Phil Plückthun <phil@plckthn.me>
2016-02-09 20:33:07 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DataWallet's babel-preset
+
+> Babel presets for our Webpack compiled environments
+
+## Install
+
+```sh
+$ npm install --save-dev datawallet-babel-preset
+```
+
---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = {
+  plugins: [
+    // babel-preset-es2015
+    require("babel-plugin-transform-es2015-template-literals"),
+    require("babel-plugin-transform-es2015-literals"),
+    require("babel-plugin-transform-es2015-function-name"),
+    require("babel-plugin-transform-es2015-arrow-functions"),
+    require("babel-plugin-transform-es2015-block-scoped-functions"),
+    require("babel-plugin-transform-es2015-classes"),
+    require("babel-plugin-transform-es2015-object-super"),
+    require("babel-plugin-transform-es2015-shorthand-properties"),
+    require("babel-plugin-transform-es2015-computed-properties"),
+    require("babel-plugin-transform-es2015-for-of"),
+    require("babel-plugin-transform-es2015-sticky-regex"),
+    require("babel-plugin-transform-es2015-unicode-regex"),
+    require("babel-plugin-check-es2015-constants"),
+    require("babel-plugin-transform-es2015-spread"),
+    require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-destructuring"),
+    require("babel-plugin-transform-es2015-block-scoping"),
+    require("babel-plugin-transform-es2015-typeof-symbol"),
+    require("babel-plugin-transform-es2015-modules-commonjs"),
+    // NOTE: Removed babel-plugin-transform-regenerator, babel-plugin-transform-es2015-modules-commonjs
+
+    // Custom stuff
+    require("babel-plugin-transform-decorators-legacy"),
+    require("babel-plugin-transform-runtime"),
+
+    // babel-preset-react
+    require("babel-plugin-transform-react-jsx"),
+    require("babel-plugin-syntax-jsx"),
+    require("babel-plugin-transform-react-display-name"),
+    // NOTE: Removed babel-plugin-syntax-flow, babel-plugin-syntax-jsx
+
+    // babel-preset-stage-1
+    require("babel-plugin-transform-class-constructor-call"),
+    require("babel-plugin-transform-class-properties"),
+    require("babel-plugin-transform-decorators"),
+    require("babel-plugin-transform-export-extensions"),
+
+    // babel-preset-stage-2
+    require("babel-plugin-syntax-trailing-function-commas"),
+    require("babel-plugin-transform-object-rest-spread"),
+
+    // babel-preset-stage-3
+    require("babel-plugin-transform-exponentiation-operator"),
+    // NOTE: Removed babel-plugin-transform-async-to-generator
+  ]
+};
